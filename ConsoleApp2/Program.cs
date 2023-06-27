4004d624f6ec5e1c1870b3d66013fb3bb2203304
gabri <gabri@DESKTOP-LQIKMSO>
2023-06-26 21:24:43 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿using (HttpClient cliente = new HttpClient()) {
+    try
+    {
+        string resposta = await cliente.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        Console.Write(resposta);
+    }
+
+    catch(Exception ex) {
+        Console.WriteLine($"Ocorreu o seguinte erro:{ex.Message}");
+    }
+
+}
+
